bc1d78865a0652ff4b38c1d273a5d2756954b3dd
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-11-30 00:55:23 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,28 @@
+
+const itemsEl = document.querySelectorAll('.item')
+
+
+const numberOfCategories = itemsEl.length;
+
+console.log(`Number of categories: ${numberOfCategories}`);
+
+
+// const categoryEl = document.querySelectorAll("h2")
+
+// for (let i = 0; i < categoryEl.length; i += 1){
+//     console.log(categoryEl[i].innerHTML);
+// }
+
+// const allElements = document.querySelectorAll('.item')
+
+// console.log(allElements);
+
+
+
+for (let i = 0; i < itemsEl.length; i += 1){
+    
+    console.log(`Category: ${itemsEl[i].children[0].innerHTML}`);
+    
+    console.log(`Elements: ${itemsEl[i].children[1].children.length}`);
+    
+}
