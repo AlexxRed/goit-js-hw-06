8760ca83458e0ed0e179797592b159bc0afda5c8
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-03 23:58:04 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,9 +1,47 @@
 const refs = {
-    form: document.querySelector('login-form'),
-    email: document.querySelector('.input'),
-    password: document.querySelector('form input'),
+    form: document.querySelector('.login-form'),
     button: document.querySelector('button')
+
 }
 
-console.log(refs.email);
-console.log(refs.password);
+const onFormSubmit = (event) =>{
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email;
+    const password = formElements.password;
+
+    if (email.value === '') {
+        alert('All fields must be filled');
+
+    } if (password.value === '') {
+        alert('All fields must be filled');
+    }
+    else {
+        const userEmail = email.value;
+        const userPassword = password.value
+
+        const allData = {
+            userEmail,
+            userPassword,
+        };
+
+        console.log(allData);
+
+        // Это другой вариант для отправки данных
+
+
+        const formData = new FormData(event.currentTarget);
+        formData.forEach((value, name) => {
+            console.log(name);
+            console.log(value);
+            
+        })
+    }
+    
+    refs.form.reset()
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+
