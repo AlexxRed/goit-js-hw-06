f87c3e6d867ab67484e0d8b52ec0e2a1dbe7f0f1
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-05 00:09:52 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// const createBoxes(amount) => {
+//   const arr = Array.from()
+
+// }
+
+console.log(Array.from({length: 50 }).fill(1));
