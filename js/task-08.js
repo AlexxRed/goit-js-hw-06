c2f56c7ac0ed7100fe4be74b17ac48e1152f07f7
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-04 11:30:37 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -7,39 +7,34 @@ const refs = {
 const onFormSubmit = (event) =>{
     event.preventDefault();
 
-    const formElements = event.currentTarget.elements;
-    const email = formElements.email;
-    const password = formElements.password;
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
 
-    if (email.value === '') {
-        alert('All fields must be filled');
-
-    } if (password.value === '') {
+    if (email === '' || password === '') {
         alert('All fields must be filled');
     }
     else {
-        const userEmail = email.value;
-        const userPassword = password.value
+        const userEmail = email;
+        const userPassword = password;
 
         const allData = {
             userEmail,
             userPassword,
         };
-
         console.log(allData);
 
         // Это другой вариант для отправки данных
 
-
+        const dataFromForm = {};
         const formData = new FormData(event.currentTarget);
+
         formData.forEach((value, name) => {
-            console.log(name);
-            console.log(value);
-            
+            dataFromForm[name] = value;
         })
+        console.log(dataFromForm);
+
+        refs.form.reset()
     }
-    
-    refs.form.reset()
 }
 
 refs.form.addEventListener('submit', onFormSubmit);
