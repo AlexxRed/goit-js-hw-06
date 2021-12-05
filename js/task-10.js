fd31994bdf101c7d140478778bfe706b6ae48f52
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-05 21:28:07 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,23 +2,43 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-
 const refs = {
-  input: document.querySelector('#controls + input'),
+  input: document.querySelector('input'),
   buttonCreate: document.querySelector('[data-create]'),
   buttonDestroy: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes')
 };
 
-refs.input.addEventListener('input', );
-refs.buttonCreate.addEventListener('click', createBoxes)
-
-
-const arrNumbersOfDiv = Array.from({length: 50 }).fill(1))
-
 const createBoxes = (amount) => {
-  
+  const inputValue = amount.target.value;
 
-}
+  const arrNumbersOfDiv = Array.from({ length: inputValue }).fill('<div></div>');
+  // const markUp= [];
+  // const markupAdd = arrNumbersOfDiv.reduce((acc, element, index, arr) => {
+  // markUp.push(`<div  style="width: ${acc+10}px; height: ${acc+10}px; background-color: ${getRandomHexColor()};"></div>`)
+  // },20)
+  // console.log(markup);
 
-console.log(Array.from({length: 50 }).fill(1));
+  const markUp = [];
+  let totalWidth = 20;
+  let totalHeight = 20;
+
+  for (let i = 0; i < arrNumbersOfDiv.length; i += 1) {
+    markUp.push(`<div  style="width: ${totalWidth += 10}px; height: ${totalHeight += 10}px; background-color: ${getRandomHexColor()};"></div>`)
+  };
+
+  const onAddBoxes = () => {
+    const fullmarkUp = markUp.join('');
+    refs.boxes.innerHTML = fullmarkUp
+  };
+
+  const onDestroyBoxes = () => {
+    refs.boxes.innerHTML = '';
+  }
+
+  refs.buttonCreate.addEventListener('click', onAddBoxes);
+  refs.buttonDestroy.addEventListener('click', onDestroyBoxes);
+
+};
+
+refs.input.addEventListener('input', createBoxes);
