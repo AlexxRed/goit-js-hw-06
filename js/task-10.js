041996c3fcc5f067280b697f3e2a12318dede14a
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-05 11:57:58 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,9 +2,22 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// const createBoxes(amount) => {
-//   const arr = Array.from()
 
-// }
+
+const refs = {
+  input: document.querySelector('#controls + input'),
+  buttonCreate: document.querySelector('[data-create]'),
+  buttonDestroy: document.querySelector('[data-destroy]'),
+};
+
+refs.input.addEventListener('input', createBoxes)
+
+
+const arrNumbersOfDiv = Array.from({length: 50 }).fill(1))
+
+const createBoxes = (amount) => {
+  
+
+}
 
 console.log(Array.from({length: 50 }).fill(1));
